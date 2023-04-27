997e65d0f0ccf021026298074ad1d09ee39a8bc2
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-04-27 00:10:40 -0400

    deleted project

--- Skill-Sharpening-Project/index.js
+++
@@ -1,7 +0,0 @@
-/*
-  This file is a place where you can test out your code without running the tests. To run this file, go to a "Console" in Replit and then type the following command exactly:
-
-  node index.js
-
-  You should copy over any function you wish to test. You will also need to call that function with data to see it work.
-*/
